230e533f35c3596628a958308787c7e6e0d8546c
Damian Malysza <damian.malysza@gmail.com>
2020-10-30 02:11:26 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -17,7 +17,7 @@ class CashRegister
   end
 
   def apply_discount
-    @total -= @total * discount.to_f / 100
+    puts "After the discount, the total comes to to $#{@total -= @total * discount.to_f / 100}"
   end
 
 end
